24af88379db08ed0b5ef7d9e330bc4daf096cdf1
sandeep-svg-45 <sandeepreddylotla28@gmail.com>
2026-04-05 20:52:04 +0530

    Cleanup: Removed helper scripts

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Finance Dashboard...
-cd app
-if not exist node_modules (
-    echo Installing dependencies... this may take a moment.
-    call npm install
-)
-echo Launching server...
-npm run dev
